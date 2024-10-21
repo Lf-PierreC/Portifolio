2a98a7aff195c3f908b75b24f0663567dd20f7a0
Lf-PierreC <lfpierre.carvalho@gmail.com>
2024-10-21 16:09:10 -0300

    starting projects poo

---
+++ projects.js
@@ -0,0 +1,13 @@
+class project {
+    constructor(titulo,descricao,icone) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.imagem = imagem;
+    }
+
+    abrirModal(){
+        
+    }
+}
+
+
